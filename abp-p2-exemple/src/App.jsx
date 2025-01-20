ce78636e65840480ffe2713c134f41e226222422
Armand M <amata3@xtec.cat>
2025-01-20 19:15:31 +0100

    1

---
+++ abp-p2-exemple/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'    NO CAL EN AQUEST CAS
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hola Bon dia!!</h1>
+    </>
+ 
+  )
+}
+
+export default App
